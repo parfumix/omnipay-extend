2bbd35d07dd3adc3492696667220cfa02b62852a
Radu <radu.bordei89@gmail.com>
2016-12-07 00:35:25 +0200

    adding set / get payment / plan id.

--- src/Common/Message/AbstractRequest.php
+++ src/Common/Message/AbstractRequest.php
@@ -4,4 +4,23 @@ namespace Omnipay\Extend\Common\Message;
 
 abstract class AbstractRequest extends \Omnipay\Common\Message\AbstractRequest {
 
+	/**
+	 * Set payment method .
+	 *
+	 * @param $value
+	 * @return mixed
+	 */
+	public function setPaymentMethod($value) {
+		return $this->setParameter('paymentMethod', $value);
+
+	}
+
+	/**
+	 * Get payment method .
+	 *
+	 * @return mixed
+	 */
+	public function getPaymentMethod() {
+		return $this->getParameter('paymentMethod');
+	}
 }
--- src/Common/Message/AbstractResponse.php
+++ src/Common/Message/AbstractResponse.php
@@ -4,4 +4,23 @@ namespace Omnipay\Extend\Common\Message;
 
 abstract class AbstractResponse extends \Omnipay\Common\Message\AbstractResponse {
 
+	/**
+	 * Set plan id method .
+	 *
+	 * @param $value
+	 * @return mixed
+	 */
+	public function setPlanId($value) {
+		return $this->setParameter('plan_id', $value);
+
+	}
+
+	/**
+	 * Get plan id method .
+	 *
+	 * @return mixed
+	 */
+	public function getPlanId() {
+		return $this->getParameter('plan_id');
+	}
 }
