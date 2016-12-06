6df61c0ca9cdfca05ccf5d73237a41bda0b17725
Radu <radu.bordei89@gmail.com>
2016-12-07 01:14:24 +0200

    adding language / coupon set / get.

--- src/Common/Message/AbstractRequest.php
+++ src/Common/Message/AbstractRequest.php
@@ -23,4 +23,44 @@ abstract class AbstractRequest extends \Omnipay\Common\Message\AbstractRequest {
 	public function getPaymentMethod() {
 		return $this->getParameter('paymentMethod');
 	}
+
+	/**
+	 * Getter: checkout language.
+	 *
+	 * @return string
+	 */
+	public function getLanguage() {
+		return $this->getParameter('language');
+	}
+
+	/**
+	 * Setter: checkout language.
+	 *
+	 * @param $value
+	 *
+	 * @return $this
+	 */
+	public function setLanguage($value) {
+		return $this->setParameter('language', $value);
+	}
+
+	/**
+	 * Getter: coupon.
+	 *
+	 * @return string
+	 */
+	public function getCoupon() {
+		return $this->getParameter('coupon');
+	}
+
+	/**
+	 * Setter: coupon.
+	 *
+	 * @param $value
+	 *
+	 * @return $this
+	 */
+	public function setCoupon($value) {
+		return $this->setParameter('coupon', $value);
+	}
 }
